9ff228d318d64e7274fc84f27009b4835dbe39d3
e.dyakov <e3001dk@gmail.com>
2022-12-06 00:33:37 +0300

    написал ф-ию для генерации случайного числа и проверке длины строки

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomInt(3, 5);
+
+function checkStringLength (string, length) {
+  if (string.length > length) {
+    return false;
+  }
+  return true;
+}
+
+checkStringLength('hi', 1);
