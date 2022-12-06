4fa4d5f12f56abff4774d7ebf3ef2b43e94acfac
e.dyakov <e3001dk@gmail.com>
2022-12-07 01:07:43 +0300

    написал ф-ию по генерации id-шников

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@ function checkStringLength (string, length) {
 }
 
 checkStringLength('hi', 1);
+
+function getIds (quantity) {
+  const ids = [];
+
+  for (let i = 1; i <= quantity; i++) {
+    const id = i;
+    ids.push(id);
+  }
+
+  return ids;
+}
+
+function getPhotos (quantity) {
+  const photos = [];
+  const ids = getIds(quantity);
+
+  for (let i = 0; i < quantity; i++) {
+    const photo = {
+      id: ids[i],
+    };
+
+    photos.push(photo);
+  }
+
+  return photos;
+}
+
+console.log(getPhotos(25));
