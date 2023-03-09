82819494846c94e152c1e717a14b1ee2e22ae6a4
e.dyakov <e3001dk@gmail.com>
2023-03-09 21:51:00 +0200

    написал валидацию формы

--- js/run-form.js
+++ js/run-form.js
@@ -5,6 +5,63 @@ const formCancel = form.querySelector('.img-upload__cancel');
 const hashtags = form.querySelector('.text__hashtags');
 const description = form.querySelector('.text__description');
 
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'form-error',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'text-help'
+});
+
+function getUserHashtags (value) {
+  const userHashtags = [];
+  value.split(' ').forEach((el) => {
+    if (el !== '' && el !== '#') {
+      userHashtags.push(el);
+    }
+  });
+  return userHashtags;
+}
+
+pristine.addValidator(hashtags, (value) => {
+  const userHashtags = getUserHashtags(value);
+  const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+  for (let i = 0; i < userHashtags.length; i++) {
+    if (!re.test(userHashtags[i])) {
+      return false;
+    }
+  }
+  return true;
+}, 'начинается с #, без спецсимволов, не более 20 знаков', 3, true);
+
+pristine.addValidator(hashtags, (value) => {
+  const userHashtags = getUserHashtags(value);
+  for (let i = 0; i < userHashtags.length - 1; i++) {
+    for (let j = i + 1; j < userHashtags.length; j++) {
+      if (userHashtags[i].toLowerCase() === userHashtags[j].toLowerCase()) {
+        return false;
+      }
+    }
+  }
+  return true;
+}, 'хэштег не может быть использован дважды', 2, true);
+
+pristine.addValidator(hashtags, (value) => {
+  const userHashtags = getUserHashtags(value);
+  if (userHashtags.length > 5) {
+    return false;
+  }
+  return true;
+}, 'не больше 5', 1, true);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    alert('валидно');
+  } else {
+    alert('не валидно');
+  }
+});
+
 function openForm () {
   overlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
